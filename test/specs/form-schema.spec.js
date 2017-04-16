282e29eb49c5bd09ea12c0b2671b4a57d7ef77c7
Sébastien Demanou <demsking@gmail.com>
2017-04-16 08:26:05 +0200

    Distinct required inputs with an asterisk

--- test/specs/form-schema.spec.js
+++ test/specs/form-schema.spec.js
@@ -47,63 +47,71 @@ describe('FormSchema', () => {
     expect(defaultData.fields.length).toBe(0)
     expect(defaultData.error).toBe(null)
   })
+})
 
-  // Inspect the component instance on mount
-  it('should correctly set props', () => {
-    // Extend the component to get the constructor, which we can then initialize directly.
-    const Constructor = Vue.extend(FormSchema)
-    const model = {}
-    const component = new Constructor({
-      propsData: {
-        schema: SignupSchema,
-        model: model
-      }
-    }).$mount()
+describe('FormSchema: inputs validation', () => {
+  // Extend the component to get the constructor, which we can then initialize directly.
+  const Constructor = Vue.extend(FormSchema)
+  const model = {}
+  const component = new Constructor({
+    propsData: {
+      schema: SignupSchema,
+      model: model
+    }
+  }).$mount()
 
-    const form = component.$el.getElementsByTagName('form')[0]
-    const inputs = form.elements
-    const button = form.getElementsByTagName('button')[0]
+  const form = component.$el.getElementsByTagName('form')[0]
+  const inputs = form.elements
+  const button = form.getElementsByTagName('button')[0]
 
-    const attr = (input, name) => input.getAttribute(name)
+  const attr = (input, name) => input.getAttribute(name)
 
-    for (let fieldName in SignupSchema.properties) {
-      const field = SignupSchema.properties[fieldName]
+  for (let fieldName in SignupSchema.properties) {
+    const field = SignupSchema.properties[fieldName]
 
-      if (field.visible === false) {
-        expect(inputs[fieldName]).toBe(undefined)
-        continue
-      }
+    if (field.visible === false) {
+      it(`invisible input '${fieldName}' should undefined`, () =>
+        expect(inputs[fieldName]).toBe(undefined))
+      continue
+    }
 
-      const input = inputs[fieldName]
+    const input = inputs[fieldName]
 
-      if (!field.attrs) {
-        field.attrs = {}
-      }
+    if (!field.attrs) {
+      field.attrs = {}
+    }
 
-      field.attrs.name = fieldName
+    field.attrs.name = fieldName
 
-      if (field.type === 'boolean') {
-        field.attrs.type = 'checkbox'
-      }
+    if (field.type === 'boolean') {
+      field.attrs.type = 'checkbox'
+    }
 
-      if (field.minLength) {
-        field.attrs.minlength = field.minLength
-      }
+    if (field.minLength) {
+      field.attrs.minlength = field.minLength
+    }
 
-      if (field.maxLength) {
-        field.attrs.maxlength = field.maxLength
-      }
+    if (field.maxLength) {
+      field.attrs.maxlength = field.maxLength
+    }
 
-      if (field.required) {
-        field.attrs.required = true
-      }
+    if (field.required) {
+      field.attrs.required = true
 
-      for (let attrName in field.attrs) {
-        expect(attr(input, attrName)).toBe(`${field.attrs[attrName]}`)
+      if (field.attrs.placeholder) {
+//         field.attrs.placeholder += ' *'
       }
     }
 
-    expect(attr(button, 'type')).toBe('submit')
-    expect(button.innerText).toBe('Submit')
-  })
+    for (let attrName in field.attrs) {
+      it(`input '${fieldName}' should have attribute '${attrName}'`, () =>
+        expect(attr(input, attrName)).toMatch(new RegExp(`${field.attrs[attrName]}`)))
+    }
+  }
+
+  it('should have a submit button', () =>
+    expect(attr(button, 'type')).toBe('submit'))
+
+  it('should have a button with Submit label', () =>
+    expect(button.innerText).toBe('Submit'))
 })
