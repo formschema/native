6c7c204b389d3e4978b41c92ed8f88b367a4d8b6
Sébastien Demanou <demsking@gmail.com>
2017-12-30 07:49:16 +0100

    Remove unnecessary console.log() call

--- build.js
+++ build.js
@@ -38,9 +38,5 @@ rm(path.join(__dirname, 'dist'), err => {
     }
 
     console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.yellow(
-      '  Tip: built files are meant to be served over an HTTP server.\n' +
-      '  Opening index.html over file:// won\'t work.\n'
-    ))
   })
 })
