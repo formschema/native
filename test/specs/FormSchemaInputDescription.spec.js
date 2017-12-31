b25a289a3e21a976657f60402937266ed4455a17
Sébastien Demanou <demsking@gmail.com>
2017-12-31 05:33:36 +0100

    Add unit test for FormSchemaInputDescription

---
+++ test/specs/FormSchemaInputDescription.spec.js
@@ -0,0 +1,38 @@
+'use strict'
+
+import { mount } from 'vue-test-utils'
+import component from '../../src/FormSchemaInputDescription.js'
+
+/* global describe it expect */
+
+describe('component', () => {
+  it('should a functional component', () => {
+    expect(component.functional).toBe(true)
+  })
+
+  it('should successfully render the component', () => {
+    const wrapper = mount(component, {
+      context: {
+        props: {
+          field: {
+            description: 'Hello'
+          }
+        }
+      }
+    })
+
+    expect(wrapper.html()).toEqual('<small>Hello</small>')
+  })
+
+  it('should successfully render with an undefined description', () => {
+    const wrapper = mount(component, {
+      context: {
+        props: {
+          field: {}
+        }
+      }
+    })
+
+    expect(wrapper.html()).toEqual(undefined)
+  })
+})
