6a7d9eae620fd0600ecbcc0edab58b7622514d77
Sébastien Demanou <demsking@gmail.com>
2018-01-08 08:07:48 +0100

    Use host path on the container and remove nodemon

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
 FROM node:8.6.0
 
+ARG cwd=/
+
+RUN mkdir -p $cwd
+WORKDIR $cwd
+
 # copy package.json for npm install
 COPY package.json package.json
 COPY package-lock.json package-lock.json
 
-RUN npm install -g nodemon
 RUN npm install
 
 COPY . .
--- devmode.sh
+++ devmode.sh
@@ -2,7 +2,7 @@
 
 package=`node -p 'require("./package").name'`
 
-docker build -t $package .
+# docker build --build-arg cwd="$(pwd)" -t $package .
 
 # create a new session. Note the -d flag, we do not want to attach just yet!
 tmux new-session -s $package -n $package -d
@@ -10,7 +10,7 @@ tmux new-session -s $package -n $package -d
 # Adress the first pane using the -t flag.
 # for the <enter> key, use either C-m (linefeed) or C-j (newline)
 tmux send-keys -t $package:$package.0 \
-  "docker run --rm -v $(pwd)/src:/src -v $(pwd)/test:/test -v $(pwd)/coverage:/coverage $package nodemon -w /src -w /test -x './node_modules/.bin/jest --colors --roots src test'" C-j
+  "nodemon -w src -w test -x 'docker run --rm -v $(pwd)/src:$(pwd)/src -v $(pwd)/test:$(pwd)/test -v $(pwd)/coverage:$(pwd)/coverage $package $(pwd)/node_modules/.bin/jest --colors --roots $(pwd) test/specs/FormSchemaField.spec.js'" C-j
 
 # split the window *vertically*
 tmux split-window -v
@@ -19,7 +19,8 @@ tmux split-window -h -t 2
 
 # again, specifying pane 1 with '-t 1' is optional
 tmux send-keys -t 2 'cd ../vue-json-schema-demo-elementui && npm run dev' C-j
-tmux send-keys -t 3 "nodemon -w src -x 'docker run --rm -v $(pwd)/dist:/dist $package node build.js && cp -f dist/* ../vue-json-schema-demo-elementui'" C-j
+tmux send-keys -t 3 \
+  "nodemon -w src -x 'docker run --rm -v $(pwd)/src:$(pwd)/src -v $(pwd)/dist:$(pwd)/dist $package node build'" C-j
 tmux send-keys -t 1 'git status' C-j
 
 # finally attach to the session
