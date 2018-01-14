f8200d7dd9c79c0bc67e9d54d022ec890af8fe3d
Sébastien Demanou <demsking@gmail.com>
2018-01-14 08:21:03 +0100

    Update dev env

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
 FROM node:8.6.0
 
-ARG cwd=/
+ARG cwd=/app
 
 RUN mkdir -p $cwd
 WORKDIR $cwd
 
-# copy package.json for npm install
 COPY package.json package.json
 COPY package-lock.json package-lock.json
 
+RUN npm install -g nodemon
 RUN npm install
 
 COPY . .
--- devmode.sh
+++ devmode.sh
@@ -1,16 +1,35 @@
 #!/bin/sh
 
-package=`node -p 'require("./package").name'`
+tag=`node -p 'require("./package").name'`
+container=$tag-dev
+cwd=$(pwd)
 
-docker build --build-arg cwd="$(pwd)" -t $package .
+if [ "X`docker images -q $tag`" = "X" ]; then
+  docker build --build-arg cwd="$cwd" -t $tag .
+fi
+
+existsOrExited=`docker inspect -f {{.State.Running}} $container 2> /dev/null`
+
+if [ "$existsOrExited" = '' ]; then
+  docker run -d -it --name $container \
+    -v "$(pwd)/src":"$cwd/src" \
+    -v "$(pwd)/test":"$cwd/test" \
+    -v "$(pwd)/coverage":"$cwd/coverage" \
+    -v "$(pwd)/dist":"$cwd/dist" \
+    $tag
+else
+  if [ "$existsOrExited" = 'false' ]; then
+    docker start $container
+  fi
+fi
 
 # create a new session. Note the -d flag, we do not want to attach just yet!
-tmux new-session -s $package -n $package -d
+tmux new-session -s $tag -n $tag -d
 
 # Adress the first pane using the -t flag.
 # for the <enter> key, use either C-m (linefeed) or C-j (newline)
-tmux send-keys -t $package:$package.0 \
-  "nodemon -w src -w test -x 'docker run --rm -v $(pwd)/src:$(pwd)/src -v $(pwd)/test:$(pwd)/test -v $(pwd)/coverage:$(pwd)/coverage $package ./node_modules/.bin/jest --colors --roots src test test/specs/FormSchemaField.spec.js'" C-j
+tmux send-keys -t $tag:$tag.0 \
+  "docker exec $container ./node_modules/.bin/jest --watchAll --colors --roots src test test/specs/FormSchemaField.spec.js" C-j
 
 # split the window *vertically*
 tmux split-window -v
@@ -19,9 +38,8 @@ tmux split-window -h -t 2
 
 # again, specifying pane 1 with '-t 1' is optional
 tmux send-keys -t 2 'cd ../vue-json-schema-demo-elementui && npm run dev' C-j
-tmux send-keys -t 3 \
-  "nodemon -w src -x 'docker run --rm -v $(pwd)/src:$(pwd)/src -v $(pwd)/dist:$(pwd)/dist $package node build'" C-j
+tmux send-keys -t 3 "docker exec $container nodemon -w src -x 'node build'" C-j
 tmux send-keys -t 1 'git status' C-j
 
 # finally attach to the session
-tmux attach -t $package
+tmux attach -t $tag
