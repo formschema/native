fa313469e19436d93bd496f2bd2908f0f3ab9aa1
Sébastien Demanou <demsking@gmail.com>
2018-02-04 11:02:14 +0100

    Fix lint

--- test/specs/FormSchemaField.spec.js
+++ test/specs/FormSchemaField.spec.js
@@ -2,7 +2,7 @@
 
 import { mount } from 'vue-test-utils'
 import { loadFields } from '../../src/lib/parser'
-import { init, components, set, initFields, inputName } from '../../src/lib/components'
+import { init, set, initFields, inputName } from '../../src/lib/components'
 
 import component from '../../src/components/FormSchemaField.js'
 
@@ -141,10 +141,6 @@ describe('component', () => {
     expect(textarea.html()).toEqual('<textarea name="fieldName">Hello</textarea>')
 
     // event
-    const expected = {
-      fieldName: 'Sébastien'
-    }
-
     textarea.element.innerHTML = 'Sébastien'
 
     expect(textarea.html()).toEqual('<textarea name="fieldName">Sébastien</textarea>')
@@ -206,10 +202,6 @@ describe('component', () => {
     expect(input.html()).toEqual('<input type="radio" name="fieldName" value="Hello">')
 
     // event
-    const expected = {
-      fieldName: 'Sébastien'
-    }
-
     input.element.value = 'Sébastien'
 
     expect(input.html()).toEqual('<input type="radio" name="fieldName" value="Sébastien">')
@@ -514,8 +506,6 @@ describe('component', () => {
 
     vm.$emit = wrapper.vm.$emit
 
-    console.log(wrapper.html())
-
     expect(wrapper.findAll('form').length).toEqual(1)
     expect(wrapper.findAll('fieldset').length).toEqual(0)
     expect(wrapper.findAll('legend').length).toEqual(0)
@@ -555,20 +545,7 @@ describe('component', () => {
         .toBe(vm.data[schema.attrs.name] === item.value)
     })
 
-//     // event
-//     const input = wrapper.find('input')
-//     const input1 = wrapper.findAll('input').at(1)
-//
-//     expect(vm.data).toEqual({ fieldName: [ 'v1' ] })
-//
-//     input.trigger('click')
-//
-//     expect(Object.keys(wrapper.emitted())).toEqual(['input', 'change'])
-//     expect(vm.data).toEqual({ fieldName: [ 'v0', 'v1' ] })
-//
-//     input1.trigger('click')
-//
-//     expect(vm.data).toEqual({ fieldName: [ 'v0' ] })
+    expect(vm.data).toEqual({ fieldName: 'v1' })
   })
 
   it('should successfully render the component with field.attrs.type === select and required field', () => {
@@ -661,7 +638,7 @@ describe('component', () => {
     }
     const vm = {
       inputValues: {
-        'fieldName': 'Hello',
+        'fieldName': 'Hello'
       },
       data: {
         fieldName: 'Hello'
@@ -703,7 +680,7 @@ describe('component', () => {
     }
     const vm = {
       inputValues: {
-        'fieldName': 'Hello',
+        'fieldName': 'Hello'
       },
       data: {
         fieldName: 'Hello'
@@ -734,201 +711,4 @@ describe('component', () => {
     expect(wrapper.emitted().input[0][0]).toEqual(expected)
     expect(vm.data).toEqual(expected)
   })
-
-/*
-  it('should successfully render a array field component', () => {
-    const field = {
-      attrs: {
-        name: 'fieldName'
-      },
-      itemsNum: 2,
-      isArrayField: true
-    }
-    const input = {
-      ref: field.attrs.name,
-      attrs: {
-        name: field.attrs.name,
-        type: 'text'
-      }
-    }
-    const vm = {
-      inputValues: {
-        'fieldName-0': 'Value 1',
-        'fieldName-1': 'Value 2'
-      },
-      data: {
-        fieldName: []
-      },
-      changed: () => {}
-    }
-    const element = components.text
-    const form = {
-      render (createElement) {
-        return createElement('form', [
-          createElement(component, {
-            props: { field, input, element, vm }
-          })
-        ])
-      }
-    }
-    const wrapper = mount(form)
-
-    expect(wrapper.findAll('form').length).toEqual(1)
-    expect(wrapper.find('form').findAll('input').length).toEqual(2)
-    expect(wrapper.find('form').findAll('button').length).toEqual(1)
-
-    const inputs = wrapper.findAll('input')
-    let i = 0
-
-    while (i < inputs.length) {
-      const input = inputs.at(i)
-      const name = `${field.attrs.name}-${i}`
-
-      expect(input.is('input')).toBe(true)
-      expect(input.isEmpty()).toBe(true)
-      expect(input.vnode.data.ref).toEqual(name)
-      expect(input.vnode.data.attrs.name).toEqual(name)
-      expect(input.vnode.data.attrs.type).toEqual('text')
-      expect(input.vnode.data.attrs.value).toEqual(vm.inputValues[name])
-      expect(Object.keys(input.vnode.data.on)).toEqual(['input', 'change'])
-
-      i++
-    }
-  })
-
-  it('should successfully render a array field for custom component', () => {
-    set('text', 'input', { type: 'text' })
-
-    const field = {
-      attrs: {
-        name: 'fieldName'
-      },
-      itemsNum: 2,
-      isArrayField: true
-    }
-    const input = {
-      ref: field.attrs.name,
-      attrs: {
-        type: 'text'
-      }
-    }
-    const vm = {
-      inputValues: {
-        'fieldName-0': 'Value 1',
-        'fieldName-1': 'Value 2'
-      },
-      data: {
-        fieldName: []
-      },
-      changed: () => {}
-    }
-    const element = components.text
-    const form = {
-      render (createElement) {
-        return createElement('form', [
-          createElement(component, {
-            props: { field, input, element, vm }
-          })
-        ])
-      }
-    }
-    const wrapper = mount(form)
-
-    expect(wrapper.findAll('form').length).toEqual(1)
-    expect(wrapper.find('form').findAll('input').length).toEqual(2)
-    expect(wrapper.find('form').findAll('button').length).toEqual(1)
-
-    const inputs = wrapper.findAll('input')
-    let i = 0
-
-    while (i < inputs.length) {
-      const input = inputs.at(i)
-      const name = `${field.attrs.name}-${i}`
-
-      expect(input.is('input')).toBe(true)
-      expect(input.isEmpty()).toBe(true)
-      expect(input.vnode.data.ref).toEqual(name)
-      expect(input.vnode.data.attrs.type).toEqual('text')
-      expect(Object.keys(input.vnode.data.on)).toEqual(['input', 'change'])
-
-      i++
-    }
-  })
-
-  it('should successfully emit the click event', () => {
-    const field = {
-      attrs: {
-        name: 'fieldName'
-      },
-      itemsNum: 2,
-      maxItems: 3,
-      isArrayField: true
-    }
-    const input = {
-      ref: field.attrs.name,
-      attrs: {
-        name: field.attrs.name,
-        type: 'password'
-      }
-    }
-    const vm = {
-      inputValues: {
-        'fieldName-0': 'Value 1',
-        'fieldName-1': 'Value 2'
-      },
-      data: {
-        fieldName: []
-      },
-      changed: () => {}
-    }
-    const element = components.password
-    const form = {
-      render (createElement) {
-        return createElement('form', [
-          createElement(component, {
-            props: { field, input, element, vm }
-          })
-        ])
-      }
-    }
-    const wrapper = mount(form)
-
-    vm.$emit = wrapper.vm.$emit
-    vm.$forceUpdate = wrapper.vm.$forceUpdate
-
-    expect(wrapper.findAll('form').length).toEqual(1)
-    expect(wrapper.find('form').findAll('input').length).toEqual(2)
-    expect(wrapper.find('form').findAll('button').length).toEqual(1)
-
-    wrapper.find('button').trigger('click')
-
-    expect(wrapper.findAll('form').length).toEqual(1)
-    expect(wrapper.find('form').findAll('input').length).toEqual(3)
-    expect(wrapper.find('form').findAll('button').length).toEqual(1)
-
-    wrapper.find('button').trigger('click')
-
-    expect(wrapper.findAll('form').length).toEqual(1)
-    expect(wrapper.find('form').findAll('input').length).toEqual(3)
-    expect(wrapper.find('form').findAll('button').length).toEqual(1)
-
-    const inputs = wrapper.findAll('input')
-    let i = 0
-
-    while (i < inputs.length) {
-      const input = inputs.at(i)
-      const name = `${field.attrs.name}-${i}`
-
-      expect(input.is('input')).toBe(true)
-      expect(input.isEmpty()).toBe(true)
-      expect(input.vnode.data.ref).toEqual(name)
-      expect(input.vnode.data.attrs.name).toEqual(name)
-      expect(input.vnode.data.attrs.type).toEqual('password')
-      expect(input.vnode.data.attrs.value).toEqual(vm.inputValues[name])
-      expect(Object.keys(input.vnode.data.on)).toEqual(['input', 'change'])
-
-      i++
-    }
-  })*/
-
 })
