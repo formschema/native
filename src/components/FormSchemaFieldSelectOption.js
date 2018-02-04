472c0b74f7478fb8f1c3ad57033046c78faff781
Sébastien Demanou <demsking@gmail.com>
2018-02-04 10:01:22 +0100

    Improve rendering of select elements

--- src/components/FormSchemaFieldSelectOption.js
+++ src/components/FormSchemaFieldSelectOption.js
@@ -3,18 +3,16 @@ import { components, input as getInput } from '../lib/components'
 export default {
   functional: true,
   render (createElement, context) {
-    const vm = context.props.vm
-    const option = context.props.option
-    const field = context.props.field
-    const attrs = field.attrs
+    const { vm, option } = context.props
 
     const input = getInput({
       vm,
       field: {
         label: option.label,
         attrs: {
-          type: attrs.type,
-          value: option.value
+          type: undefined,
+          value: option.value,
+          selected: option.selected
         }
       }
     })
