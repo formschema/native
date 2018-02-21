45ae7e6ade5bd4f8a4685328dd64ea978ce1bdeb
Sébastien Demanou <demsking@gmail.com>
2018-02-21 01:21:32 +0100

    Remove elementui reference

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,12 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en'
-
-Vue.use(ElementUI, { locale })
-
 Vue.config.productionTip = false
 
 new Vue({
