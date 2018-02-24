1daf45b565d986285f7e8eb2540a2f2f89e2f350
Sébastien Demanou <demsking@gmail.com>
2018-02-24 22:16:03 +0100

    Add isScalar(value) and merge(dest, src) functions

--- src/lib/object.js
+++ src/lib/object.js
@@ -9,3 +9,27 @@ export function equals (o1, o2) {
 
   return keys1.findIndex((key) => !o2.hasOwnProperty(key) || o1[key] !== o2[key]) === -1
 }
+
+export function isScalar (value) {
+  if (value === null) {
+    return true
+  }
+  return /string|number|boolean|undefined|function/.test(typeof value)
+}
+
+export function merge (dest, src) {
+  Object.keys(src).forEach((key) => {
+    const value = src[key]
+
+    if (isScalar(value)) {
+      dest[key] = value
+    } else if (value instanceof Array) {
+      dest[key] = [ ...value ]
+    } else {
+      if (!dest[key]) {
+        dest[key] = {}
+      }
+      merge(dest[key], value)
+    }
+  })
+}
--- test/specs/object.spec.js
+++ test/specs/object.spec.js
@@ -1,11 +1,11 @@
 'use strict'
 
-import { equals } from '../../src/lib/object'
+import { equals, isScalar, merge } from '@/lib/object'
 
 /* global describe it expect */
 
 describe('lib/object', () => {
-  describe('object.equals(o1, o2)', () => {
+  describe('equals(o1, o2)', () => {
     it('should return false for inputs with not equal keys length', () => {
       const o1 = { a: 1 }
       const o2 = { a: 1, b: 2 }
@@ -39,6 +39,32 @@ describe('lib/object', () => {
       const o2 = {}
 
       expect(equals(o1, o2)).toEqual(true)
+    })
+  })
+
+  describe('isScalar(value)', () => {
+    ['hello', 123, true, undefined, null, () => {}].forEach((value) => {
+      it(`should return true for '${value}' as scalar value`, () => {
+        expect(isScalar(value)).toBe(true)
+      })
+    });
+
+    [{}, []].forEach((value) => {
+      it(`should return false for '${JSON.stringify(value)}' as non scalar value`, () => {
+        expect(isScalar(value)).toBe(false)
+      })
+    })
+  })
+
+  describe('merge(dest, src)', () => {
+    it('should successfully merge src object to the dest object', () => {
+      const src = { a: 1, c: 2, e: { x: 1 }, d: [1], f: { g: 1 } }
+      const dest = { a: 0, b: 1, e: { y: 2 }, d: [2, 3] }
+      const expected = { a: 1, b: 1, c: 2, e: { x: 1, y: 2 }, d: [1], f: { g: 1 } }
+
+      merge(dest, src)
+
+      expect(dest).toEqual(expected)
     })
   })
 })
