b62ca4160196da61233c495f3ed41f2697b1048a
Sébastien Demanou <demsking@gmail.com>
2018-06-24 19:18:06 +0200

    Fix production build

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,3 @@
-const path = require('path')
-
 module.exports = {
-  lintOnSave: true,
-  configureWebpack: {
-    entry: path.join(__dirname, 'src/components/FormSchema.js'),
-    output: {
-      path: path.join(__dirname, 'dist'),
-      filename: 'FormSchema.js',
-      libraryTarget: 'umd',
-      library: 'FormSchema',
-      umdNamedDefine: true
-    },
-    externals: {
-      'vue': 'vue'
-    }
-  }
+  lintOnSave: true
 }
