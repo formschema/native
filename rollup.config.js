528083e083bdd84c0cfc4f927dfc1bde825d33d4
Sébastien Demanou <demsking@gmail.com>
2018-07-10 20:52:15 +0200

    Add commonjs and node-resolve rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,24 @@ import { uglify } from 'rollup-plugin-uglify'
 import babel from 'rollup-plugin-babel'
 import pkg from './package.json'
 
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+
+import sourcemaps from 'rollup-plugin-sourcemaps'
+
 const DEST = 'dist'
 const MODULE_NAME = 'FormSchema'
 
 const BANNER = `/* ${pkg.name} v${pkg.version} (c) ${pkg.author} - ${pkg.license} */`
 
 const PLUGINS = [
+  sourcemaps(),
+  resolve({
+    jsnext: true,
+    main: true,
+    browser: true
+  }),
+  commonjs(),
   babel({
     exclude: 'node_modules/**',
     babelrc: false,
@@ -25,8 +37,10 @@ const PLUGINS = [
 function build (format, suffix = `${format}.min`) {
   return {
     input: 'src/components/FormSchema.js',
+    cache: false,
     output: {
-      file: `${DEST}/${MODULE_NAME}.${suffix}.js`,
+      dir: DEST,
+      file: `${MODULE_NAME}.${suffix}.js`,
       format: format,
       name: MODULE_NAME,
       indent: false,
