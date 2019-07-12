65e280f521f0b26f39d68ee0dca3e6d1e291d8e0
Sébastien Demanou <demsking@gmail.com>
2019-07-12 06:06:34 +0200

    HelperElement: add vue tests

---
+++ tests/specs/components/HelperElement.spec.ts
@@ -0,0 +1,91 @@
+import { mount } from '@vue/test-utils';
+import { HelperElement } from '@/components/HelperElement';
+import { StringParser } from '@/parsers/StringParser';
+import { ScalarDescriptor, ParserOptions } from '@/types';
+import { NativeDescriptor } from '@/lib/NativeDescriptor';
+
+describe('components/HelperElement', () => {
+  it('should successfully render component', () => {
+    const options: ParserOptions<string, ScalarDescriptor> = {
+      schema: {
+        type: 'string',
+        description: 'Your full name'
+      },
+      model: 'Goku',
+      id: 'name',
+      descriptorConstructor: NativeDescriptor.get
+    };
+
+    const parser = new StringParser(options);
+
+    parser.parse();
+
+    const context: any = {
+      attrs: parser.field.attrs.input,
+      props: {
+        field: parser.field
+      }
+    };
+
+    const wrapper = mount(HelperElement, { context });
+    const expected = '<p id="name-desc">Your full name</p>';
+
+    expect(wrapper.html()).toBe(expected);
+  });
+
+  it('should successfully render component for non root field', () => {
+    const options: ParserOptions<string, ScalarDescriptor> = {
+      schema: {
+        type: 'string',
+        description: 'Your full name'
+      },
+      model: 'Goku',
+      id: 'name',
+      descriptorConstructor: NativeDescriptor.get
+    };
+
+    const parser = new StringParser(options);
+
+    parser.parse();
+
+    parser.field.isRoot = false;
+
+    const context: any = {
+      attrs: parser.field.attrs.input,
+      props: {
+        field: parser.field
+      }
+    };
+
+    const wrapper = mount(HelperElement, { context });
+    const expected = '<span id="name-desc">Your full name</span>';
+
+    expect(wrapper.html()).toBe(expected);
+  });
+
+  it('should render nothing with missing field.descriptor.description', () => {
+    const options: ParserOptions<string, ScalarDescriptor> = {
+      schema: {
+        type: 'string'
+      },
+      model: 'Goku',
+      id: 'name',
+      descriptorConstructor: NativeDescriptor.get
+    };
+
+    const parser = new StringParser(options);
+
+    parser.parse();
+
+    const context: any = {
+      attrs: parser.field.attrs.input,
+      props: {
+        field: parser.field
+      }
+    };
+
+    const wrapper = mount(HelperElement, { context });
+
+    expect(wrapper.html()).toBeUndefined();
+  });
+});
