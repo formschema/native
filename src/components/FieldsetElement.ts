2fa94e0edcabf109c1f53e3e2a11abecbe281d66
Sébastien Demanou <demsking@gmail.com>
2019-07-27 08:52:53 +0200

    FieldsetElement: use fieldset element only for root field

--- src/components/FieldsetElement.ts
+++ src/components/FieldsetElement.ts
@@ -1,11 +1,11 @@
 import { HelperElement } from '@/components/HelperElement';
 import { FieldsetComponent } from '@/types';
+import { FieldElement } from './FieldElement';
 
 export const FieldsetElement: FieldsetComponent = {
   name: 'FieldsetElement',
   functional: true,
   render(h, { data, props, slots }) {
-    const attrs = props.field.input.attrs;
     const nodes = slots().default || props.field.children.map((field) => {
       return h(field.input.component, {
         key: field.key,
@@ -13,6 +13,11 @@ export const FieldsetElement: FieldsetComponent = {
       });
     });
 
+    if (!props.field.isRoot) {
+      return h(FieldElement, data, nodes);
+    }
+
+    const attrs = props.field.input.attrs;
     const helper = h(HelperElement, data);
 
     if (helper.tag) {
